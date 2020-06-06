70e9905cf37119d48da8b355a989e4cf678f75cc
Evan <schafer.evan.r@gmail.com>
2020-06-06 15:28:00 -0700

    Add story form
    Added a form for people to write their stories. Accessible by
    clicking on the 'no-story' button, and then the 'share your story'
    button.

---
+++ client/components/StoryForm.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+class StoryForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.onStorySubmit = this.onStorySubmit.bind(this);
+  }
+
+  onStorySubmit() {
+    console.log('submitted!');
+  }
+
+  render() {
+    return (
+      <div id="story-form">
+        <div id="story-name">
+          <label>What do you call it?</label>
+          <input id="input-name" type="text"></input>
+        </div>
+        <div id="story-saw">
+          <label>What did you see?</label>
+          <input id="input-saw" type="text"></input>
+        </div>
+        <div id="story-heard">
+          <label>What did you hear?</label>
+          <input id="input-heard" type="text"></input>
+        </div>
+        <div id="story-body">
+          <label id="story-body">Tell us your story</label>
+          <input id="input-story" type="text"></input>
+        </div>
+        <div id="submit-zone">
+          <button onClick={this.onStorySubmit}>Submit your story!</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default StoryForm;
