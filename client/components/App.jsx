5bd78b5bcdd00374c20c8699609a0cbf7801b481
Evan <schafer.evan.r@gmail.com>
2020-06-06 14:46:44 -0700

    Add placeholder for clicking on locations
    clicking on placeholder1 brings up location info placeholder and
    absent story placeholder, and clicking on placeholder2 brings
    up location info placeholder and present story placeholder.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,18 +7,43 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      locationSelected: false,
+      storyPresent: 2,
     };
+    this.handleLocationClick = this.handleLocationClick.bind(this);
+  }
+
+  handleLocationClick() {
+    document.getElementById('location-info').classList.remove('hidden');
+    if (event.target.id === 'no-story') {
+      document.getElementById('absent-story').classList.remove('hidden');
+      document.getElementById('present-story').classList.add('hidden');
+      this.setState({
+        locationSelected: true,
+        storyPresent: 0,
+      });
+    } else if (event.target.id === 'story') {
+      document.getElementById('present-story').classList.remove('hidden');
+      document.getElementById('absent-story').classList.add('hidden');
+      this.setState({
+        locationSelected: true,
+        storyPresent: 1,
+      });
+    }
   }
 
   render() {
     return (
       <div>
-        <h1>hello world 2</h1>
-        <LocationInfo />
-        <div>
+        <h1 id="no-story" onClick={this.handleLocationClick}>location: no story</h1>
+        <h1 id="story" onClick={this.handleLocationClick}>location: story</h1>
+        <div className="hidden" id="location-info">
+          <LocationInfo />
+        </div>
+        <div className="hidden" id="absent-story">
           <AbsentStory />
         </div>
-        <div>
+        <div className="hidden" id="present-story">
           <PresentStory />
         </div>
       </div>
