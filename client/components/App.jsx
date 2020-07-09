1e5e3626b7c1d4889f964d04c8b4182136e352c9
Evan <schafer.evan.r@gmail.com>
2020-07-08 19:47:15 -0700

    Add ability to retrieve substories from database
    Clicking on a map marker and then clicking the show files button
    now retrieves substories related to the main story and stores them
    in state. Next scheduled features are to:
    - display number of substories found
    - display substories in a clickable list
    - display an individual substory when its name is clicked

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -14,6 +14,7 @@ class App extends React.Component {
       storyPresent: 2,
       lastMarker: undefined,
       currentStory: {
+        storyId: 'REDACTED',
         story: 'REDACTED',
         storylocation: 'REDACTED',
         storyname: 'REDACTED',
