9cf2f8104164cabb84d1040f3711a5c6df7e5dcb
Evan <schafer.evan.r@gmail.com>
2020-08-13 17:22:34 -0700

    Refactor like and dislike
    Refactored the like and dislike and dislike buttons for the
    substories to be housed in their own separate jsx files, and
    additionally changed them to activate based on whether or not
    a user is logged in. Trying to like or dislike a story while
    not logged in will display a prompt asking the user to log in.
    
    Additional changes:
    -changed the database schema to have tables for users, story
    likes, and substory likes
    -added an 'app map' file that visually shows how the application
    is set up file-structure-wise

--- client/components/SubStoryRender.jsx
+++ client/components/SubStoryRender.jsx
@@ -1,105 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Axios from 'axios';
+import SubStoryLike from './SubStoryRender-Interactions/SubStoryLike';
+import SubStoryDislike from './SubStoryRender-Interactions/SubStoryDislike';
 
-class SubStoryRender extends React.Component {
-  constructor(props) {
-    super(props);
-    this.like = this.like.bind(this);
-    this.dislike = this.dislike.bind(this);
-  }
+// <p id="like-click" role="presentation" className="underlined clickable" onClick={this.like}>like</p>
+// <p id="dislike-click" role="presentation" className="underlined clickable" onClick={this.dislike}>dislike</p>
 
-  like() {
-    if (!document.getElementById('like-click').classList.contains('disabled')) {
-      const { subStory, likeGoUp } = this.props;
-      Axios.put('/subLike', {
-        id: subStory.id,
-      })
-        .then((result) => {
-          console.log(result);
-          likeGoUp();
-          document.getElementById('like-click').classList.add('disabled');
-          document.getElementById('dislike-click').classList.remove('disabled');
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }
-
-  dislike() {
-    if (!document.getElementById('dislike-click').classList.contains('disabled')) {
-      const { subStory, likeGoDown } = this.props;
-      Axios.put('/subDislike', {
-        id: subStory.id,
-      })
-        .then((result) => {
-          console.log(result);
-          likeGoDown();
-          document.getElementById('like-click').classList.remove('disabled');
-          document.getElementById('dislike-click').classList.add('disabled');
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }
-
-  render() {
-    const { subStory } = this.props;
-    return (
-      <div>
-        <div id="story-header">
-          <div>
-            <h5>Department of Weird Stuff</h5>
-            <h5>
-              Case No°
-              { ' ' }
-              {10000 + subStory.storyid}
-              =
-              {subStory.id}
-            </h5>
-          </div>
-        </div>
-        <div id="story-classified">
-          <div>
-            <h1>CLASSIFIED</h1>
-          </div>
-        </div>
-        <div id="substory-likes">
-          <h4>
-            likes:
+const SubStoryRender = (props) => {
+  const { subStory, likeGoDown, likeGoUp } = props;
+  return (
+    <div>
+      <div id="story-header">
+        <div>
+          <h5>Department of Weird Stuff</h5>
+          <h5>
+            Case No°
             { ' ' }
-            {subStory.sublikes}
-          </h4>
-          <p id="like-click" role="presentation" className="underlined clickable" onClick={this.like}>like</p>
-          <p id="dislike-click" role="presentation" className="underlined clickable" onClick={this.dislike}>dislike</p>
-        </div>
-        <div>
-          <h3 className="underlined">INCIDENT NAME:</h3>
-          {subStory.subname}
-        </div>
-        <div>
-          <h3 className="underlined">INCIDENT LOCATION:</h3>
-          {subStory.sublocation}
-        </div>
-        <div>
-          <h3 className="underlined">WITNESS DESCRIPTION:</h3>
-          {subStory.subseen}
-          {subStory.subheard}
-        </div>
-        <div>
-          <h3 className="underlined">TIMELINE OF EVENTS:</h3>
-          {subStory.substory}
-        </div>
-        <div id="secret-footer">
-          <h5 className="top-secret">TOP SECRET</h5>
+            {10000 + subStory.storyid}
+            =
+            {subStory.id}
+          </h5>
         </div>
       </div>
-    );
-  }
-}
-
+      <div id="story-classified">
+        <div>
+          <h1>CLASSIFIED</h1>
+        </div>
+      </div>
+      <div id="substory-likes">
+        <h4>
+          likes:
+          { ' ' }
+          {subStory.sublikes}
+        </h4>
+        <SubStoryLike subStory={subStory} like={likeGoUp} />
+        <SubStoryDislike subStory={subStory} dislike={likeGoDown} />
+      </div>
+      <div>
+        <h3 className="underlined">INCIDENT NAME:</h3>
+        {subStory.subname}
+      </div>
+      <div>
+        <h3 className="underlined">INCIDENT LOCATION:</h3>
+        {subStory.sublocation}
+      </div>
+      <div>
+        <h3 className="underlined">WITNESS DESCRIPTION:</h3>
+        {subStory.subseen}
+        {subStory.subheard}
+      </div>
+      <div>
+        <h3 className="underlined">TIMELINE OF EVENTS:</h3>
+        {subStory.substory}
+      </div>
+      <div id="secret-footer">
+        <h5 className="top-secret">TOP SECRET</h5>
+      </div>
+    </div>
+  );
+};
 SubStoryRender.propTypes = {
   subStory: PropTypes.isRequired,
   likeGoUp: PropTypes.isRequired,
