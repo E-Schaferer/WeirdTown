ff666043c66e3bf6f45b2f3420c6d6691fa90c40
Evan <schafer.evan.r@gmail.com>
2020-08-26 13:19:41 -0700

    Replace mount with shallow
    replaced mount rendering of App in testing with shallow

--- client/testing/app.test.jsx
+++ client/testing/app.test.jsx
@@ -1,34 +1,32 @@
 import React from 'react';
-import { mount, configure } from 'enzyme';
+import { mount, configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../components/App';
 
 configure({ adapter: new Adapter() });
 
 describe('app tests', () => {
-  const docWrapper = mount(<App />, { attachTo: document.body });
   it('should render', () => {
-    expect(docWrapper).toBeTruthy();
+    const wrapper = shallow(<App />);
+    expect(wrapper).toBeTruthy();
   });
   it('should update absentStoryRender and presentStoryRender when handleLocationClick runs, and to properly add input location to state', () => {
-    docWrapper.setState({
-      absentStoryRender: false,
-      presentStoryRender: false,
-    });
-    docWrapper.instance().handleLocationClick([100, 50]);
-    expect(docWrapper.instance().state.lastMarker).toContain(100);
-    expect(docWrapper.instance().state.absentStoryRender).toBe(true);
-    expect(docWrapper.instance().state.presentStoryRender).toBe(false);
+    const wrapper = shallow(<App />);
+    wrapper.instance().handleLocationClick([100, 50]);
+    wrapper.update();
+    expect(wrapper.instance().state.lastMarker).toContain(100);
+    expect(wrapper.instance().state.absentStoryRender).toBe(true);
+    expect(wrapper.instance().state.presentStoryRender).toBe(false);
   });
   it('should not render elements before buttons rendering them are clicked', () => {
-    const wrapper = mount(<App />);
+    const wrapper = shallow(<App />);
     expect(wrapper.find('#auth-zone').exists()).toBeTruthy();
     expect(wrapper.find('#map-zone').exists()).toBeTruthy();
     expect(wrapper.find('#absent-story').exists()).toBeFalsy();
     expect(wrapper.find('#present-story').exists()).toBeFalsy();
   });
   it('should render elements after buttons rendering them are clicked', () => {
-    const wrapper = mount(<App />);
+    const wrapper = shallow(<App />);
     wrapper.instance().handleLocationClick([100, 50]);
     wrapper.update();
     expect(wrapper.instance().state.absentStoryRender).toBe(true);
@@ -36,7 +34,7 @@ describe('app tests', () => {
     expect(wrapper.find('#present-story').exists()).toBeFalsy();
   });
   it('should update state after onShowSub resolves', () => {
-    const wrapper = mount(<App />);
+    const wrapper = shallow(<App />);
     wrapper.instance().onShowSub();
     wrapper.update();
     expect(wrapper.instance().state.subStory).toBe(true);
@@ -46,7 +44,7 @@ describe('app tests', () => {
     expect(wrapper.instance().state.subStoryPrompt).toBe(false);
   });
   it('should update state after onShowSubStories resolves', () => {
-    const wrapper = mount(<App />);
+    const wrapper = shallow(<App />);
     wrapper.instance().onShowSubStories();
     wrapper.update();
     expect(wrapper.instance().state.subStoryListZone).toBe(true);
@@ -56,28 +54,28 @@ describe('app tests', () => {
     expect(wrapper.instance().state.subStoryRenderZone).toBe(false);
   });
   it('should update state after onShowSubForm resolves', () => {
-    const wrapper = mount(<App />);
+    const wrapper = shallow(<App />);
     wrapper.instance().onShowSubForm();
     wrapper.update();
     expect(wrapper.instance().state.subStoryForm).toBe(true);
     expect(wrapper.instance().state.subStoryButton).toBe(false);
   });
   it('should update state after onSubStoryListItemClick resolves', () => {
-    const wrapper = mount(<App />);
+    const wrapper = shallow(<App />);
     wrapper.instance().onSubStoryListItemClick();
     wrapper.update();
     expect(wrapper.instance().state.subStoryRenderZone).toBe(true);
     expect(wrapper.instance().state.subStorySubList).toBe(false);
   });
   it('should update state after onGoBack revolves', () => {
-    const wrapper = mount(<App />);
+    const wrapper = shallow(<App />);
     wrapper.instance().onGoBack();
     wrapper.update();
     expect(wrapper.instance().state.subStorySubList).toBe(true);
     expect(wrapper.instance().state.subStoryRenderZone).toBe(false);
   });
   it('should update state after storyFormRender resolves', () => {
-    const wrapper = mount(<App />);
+    const wrapper = shallow(<App />);
     wrapper.instance().storyFormRender();
     wrapper.update();
     expect(wrapper.instance().state.storyFormZone).toBe(true);
