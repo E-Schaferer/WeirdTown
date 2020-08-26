693347726a97f45670ec82070c94fd54c5783554
Evan <schafer.evan.r@gmail.com>
2020-08-26 12:58:51 -0700

    Add state tests
    Added tests for App.jsx to make sure state is properly updating
    when App methods are called.

--- client/testing/app.test.jsx
+++ client/testing/app.test.jsx
@@ -35,4 +35,52 @@ describe('app tests', () => {
     expect(wrapper.find('#absent-story').exists()).toBeTruthy();
     expect(wrapper.find('#present-story').exists()).toBeFalsy();
   });
+  it('should update state after onShowSub resolves', () => {
+    const wrapper = mount(<App />);
+    wrapper.instance().onShowSub();
+    wrapper.update();
+    expect(wrapper.instance().state.subStory).toBe(true);
+    expect(wrapper.instance().state.subStoryList).toBe(true);
+    expect(wrapper.instance().state.subStoryListButtonFlex).toBe(true);
+    expect(wrapper.instance().state.subStoryButton).toBe(true);
+    expect(wrapper.instance().state.subStoryPrompt).toBe(false);
+  });
+  it('should update state after onShowSubStories resolves', () => {
+    const wrapper = mount(<App />);
+    wrapper.instance().onShowSubStories();
+    wrapper.update();
+    expect(wrapper.instance().state.subStoryListZone).toBe(true);
+    expect(wrapper.instance().state.subStorySubList).toBe(true);
+    expect(wrapper.instance().state.subStoryList).toBe(false);
+    expect(wrapper.instance().state.subStoryListButtonFlex).toBe(false);
+    expect(wrapper.instance().state.subStoryRenderZone).toBe(false);
+  });
+  it('should update state after onShowSubForm resolves', () => {
+    const wrapper = mount(<App />);
+    wrapper.instance().onShowSubForm();
+    wrapper.update();
+    expect(wrapper.instance().state.subStoryForm).toBe(true);
+    expect(wrapper.instance().state.subStoryButton).toBe(false);
+  });
+  it('should update state after onSubStoryListItemClick resolves', () => {
+    const wrapper = mount(<App />);
+    wrapper.instance().onSubStoryListItemClick();
+    wrapper.update();
+    expect(wrapper.instance().state.subStoryRenderZone).toBe(true);
+    expect(wrapper.instance().state.subStorySubList).toBe(false);
+  });
+  it('should update state after onGoBack revolves', () => {
+    const wrapper = mount(<App />);
+    wrapper.instance().onGoBack();
+    wrapper.update();
+    expect(wrapper.instance().state.subStorySubList).toBe(true);
+    expect(wrapper.instance().state.subStoryRenderZone).toBe(false);
+  });
+  it('should update state after storyFormRender resolves', () => {
+    const wrapper = mount(<App />);
+    wrapper.instance().storyFormRender();
+    wrapper.update();
+    expect(wrapper.instance().state.storyFormZone).toBe(true);
+    expect(wrapper.instance().state.storySubmissionPrompt).toBe(false);
+  });
 });
