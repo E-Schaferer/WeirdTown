73429c034b632b24af636eb34cd28d5c238bd7b6
Evan <schafer.evan.r@gmail.com>
2020-09-17 13:55:42 -0700

    Add SubStory tests
    Add tests to SubStory to reach 100% coverage. Also minorly
    refactored SubStory to accomodate tests.

--- client/testing/SubStory.test.jsx
+++ client/testing/SubStory.test.jsx
@@ -10,4 +10,39 @@ describe('substory tests', () => {
     const wrapper = shallow(<SubStory />);
     expect(wrapper).toBeTruthy();
   });
+  it('should render the substory list if substory list is passed as true as a prop', () => {
+    const wrapper = shallow(<SubStory subStoryList />);
+    expect(wrapper.find('#substory-list').exists()).toBeTruthy();
+    expect(wrapper.find('#substory-list-button-flex-zone').exists()).toBeFalsy();
+  });
+  it('should render a unique message based on the number of substories (1)', () => {
+    const wrapper = shallow(<SubStory subStoryList subNum={1} />);
+    expect(wrapper.find('#one-addendum').exists()).toBeTruthy();
+    expect(wrapper.find('#multiple-addendums').exists()).toBeFalsy();
+  });
+  it('should render a unique message based on the number of substories (more than 1)', () => {
+    const wrapper = shallow(<SubStory subStoryList subNum={3} />);
+    expect(wrapper.find('#multiple-addendums').exists()).toBeTruthy();
+    expect(wrapper.find('#one-addendum').exists()).toBeFalsy();
+  });
+  it('should render the substory list button if substorylistbuttonflex is passed as true as a prop', () => {
+    const wrapper = shallow(<SubStory subStoryListButtonFlex />);
+    expect(wrapper.find('#substory-list-button-flex-zone').exists()).toBeTruthy();
+    expect(wrapper.find('#substory-list-zone').exists()).toBeFalsy();
+  });
+  it('should render the substory list if subStoryListZone is passed as true as a prop', () => {
+    const wrapper = shallow(<SubStory subStoryListZone />);
+    expect(wrapper.find('#substory-list-zone').exists()).toBeTruthy();
+    expect(wrapper.find('#sub-story-button').exists()).toBeFalsy();
+  });
+  it('should render the subStoryButton if subStoryButton is passed as true as a prop', () => {
+    const wrapper = shallow(<SubStory subStoryButton />);
+    expect(wrapper.find('#sub-story-button').exists()).toBeTruthy();
+    expect(wrapper.find('#sub-story-form').exists()).toBeFalsy();
+  });
+  it('should render the subStoryForm if subStoryForm is passed as true as a prop', () => {
+    const wrapper = shallow(<SubStory subStoryForm />);
+    expect(wrapper.find('#sub-story-form').exists()).toBeTruthy();
+    expect(wrapper.find('#substory-list').exists()).toBeFalsy();
+  });
 });
