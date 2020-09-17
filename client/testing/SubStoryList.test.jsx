ed3bb546baacbb16cff63f0ee64b8f673204485b
Evan <schafer.evan.r@gmail.com>
2020-09-17 15:06:26 -0700

    Add substorylist test
    Added tests to SubStoryLists to achieve 100% coverage.

--- client/testing/SubStoryList.test.jsx
+++ client/testing/SubStoryList.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import SubStoryList from '../components/SubStoryList';
+import SubStoryLike from '../components/SubStoryRender-Interactions/SubStoryLike';
 
 configure({ adapter: new Adapter() });
 
@@ -9,5 +10,35 @@ describe('substory list tests', () => {
   it('should render', () => {
     const wrapper = shallow(<SubStoryList />);
     expect(wrapper).toBeTruthy();
+  });
+  it('should increment likes when likeGoUp is called', () => {
+    const wrapper = shallow(<SubStoryList />);
+    wrapper.instance().likeGoUp();
+    expect(wrapper.instance().state.subStory.sublikes).toBe(1);
+  });
+  it('should decrement likes when likeGoDown is called', () => {
+    const wrapper = shallow(<SubStoryList />);
+    wrapper.instance().likeGoDown();
+    expect(wrapper.instance().state.subStory.sublikes).toBe(-1);
+  });
+  it('should call onSubStoryListItemClick and set subStory to the 0 index of result.data', () => {
+    const test = jest.fn();
+    const result = {
+      data: ['winner', 'loser'],
+    };
+    const wrapper = shallow(<SubStoryList onSubStoryListItemClick={test} />);
+    wrapper.instance().handleListClick(result);
+    expect(test).toHaveBeenCalled();
+    expect(wrapper.instance().state.subStory).toBe('winner');
+  });
+  it('should render the list of substories if subStorySubList is passed as true as a prop', () => {
+    const wrapper = shallow(<SubStoryList subStorySubList />);
+    expect(wrapper.find('#substory-sub-list').exists()).toBeTruthy();
+    expect(wrapper.find('#substory-render-zone').exists()).toBeFalsy();
+  });
+  it('should render the substory if subStoryRenderZone is passed as true as a prop', () => {
+    const wrapper = shallow(<SubStoryList subStoryRenderZone />);
+    expect(wrapper.find('#substory-sub-list').exists()).toBeFalsy();
+    expect(wrapper.find('#substory-render-zone').exists()).toBeTruthy();
   });
 });
