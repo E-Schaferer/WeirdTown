568247d9492f8a34456ebd72b6e22b37d71ff03e
Evan <schafer.evan.r@gmail.com>
2020-09-23 15:43:19 -0700

    Refactor story form submit
    Refactored StoryFormSubmit to handle textarea values in state
    rather than in dom elements. Added method to app.jsx to set
    state.

--- client/components/StoryForm.jsx
+++ client/components/StoryForm.jsx
@@ -3,7 +3,15 @@ import PropTypes from 'prop-types';
 import StoryFormSubmit from './StoryForm-Interactions/StoryFormSubmit';
 
 const StoryForm = (props) => {
-  const { storyFormSubmit } = props;
+  const {
+    storyFormSubmit,
+    inputName,
+    inputLocation,
+    inputSaw,
+    inputHeard,
+    inputStory,
+    handleTextAreaChange,
+  } = props;
   return (
     <div id="story-form">
       <div id="input-weird-header">
@@ -15,35 +23,42 @@ const StoryForm = (props) => {
       <div id="story-name-input">
         <label htmlFor="name">
           What do you call it?
-          <input name="name" id="input-name" type="text" />
+          <input name="name" id="inputName" onChange={handleTextAreaChange} type="text" />
         </label>
       </div>
       <div>
         <label htmlFor="location">
           Where did it happen?
-          <textarea name="location" rows="5" cols="75" id="input-location" type="text" />
+          <textarea name="location" rows="5" cols="75" id="inputLocation" onChange={handleTextAreaChange} type="text" />
         </label>
       </div>
       <div id="story-saw">
         <label htmlFor="saw">
           What did you see?
-          <textarea name="saw" rows="5" cols="75" id="input-saw" type="text" />
+          <textarea name="saw" rows="5" cols="75" id="inputSaw" onChange={handleTextAreaChange} type="text" />
         </label>
       </div>
       <div id="story-heard">
         <label htmlFor="heard">
           What did you hear?
-          <textarea name="heard" rows="5" cols="75" id="input-heard" type="text" />
+          <textarea name="heard" rows="5" cols="75" id="inputHeard" onChange={handleTextAreaChange} type="text" />
         </label>
       </div>
       <div id="story-body">
         <label id="story-body" htmlFor="body">
           Tell us your story
-          <textarea name="body" rows="5" cols="75" id="input-story" type="text" />
+          <textarea name="body" rows="5" cols="75" id="inputStory" onChange={handleTextAreaChange} type="text" />
         </label>
       </div>
       <div id="submit-zone">
-        <StoryFormSubmit storyFormSubmit={storyFormSubmit} />
+        <StoryFormSubmit
+          storyFormSubmit={storyFormSubmit}
+          inputName={inputName}
+          inputLocation={inputLocation}
+          inputSaw={inputSaw}
+          inputHeard={inputHeard}
+          inputStory={inputStory}
+        />
       </div>
       <div id="secret-footer">
         <h5 className="top-secret">TOP SECRET</h5>
@@ -53,9 +68,21 @@ const StoryForm = (props) => {
 };
 StoryForm.propTypes = {
   storyFormSubmit: PropTypes.func,
+  handleTextAreaChange: PropTypes.func,
+  inputName: PropTypes.string,
+  inputLocation: PropTypes.string,
+  inputSaw: PropTypes.string,
+  inputHeard: PropTypes.string,
+  inputStory: PropTypes.string,
 };
 StoryForm.defaultProps = {
   storyFormSubmit: undefined,
+  handleTextAreaChange: undefined,
+  inputName: '',
+  inputLocation: '',
+  inputSaw: '',
+  inputHeard: '',
+  inputStory: '',
 };
 
 export default StoryForm;
