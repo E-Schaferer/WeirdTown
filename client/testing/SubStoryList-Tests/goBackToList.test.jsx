8025c21d97b354b22a3c0c795012667b069efae2
Evan <schafer.evan.r@gmail.com>
2021-01-01 16:48:13 -0800

    Fix broken test
    - fixed a test that was broken by renaming goBack to
    goBackToList

--- client/testing/SubStoryList-Tests/goBackToList.test.jsx
+++ client/testing/SubStoryList-Tests/goBackToList.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import GoBack from '../../components/SubStoryList/GoBack';
+import GoBackToList from '../../components/SubStoryList/GoBackToList';
 
 configure({ adapter: new Adapter() });
 
